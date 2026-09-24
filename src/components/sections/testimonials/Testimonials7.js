c7f0cf830ccc40fa2c0072ca2e46ffed8a0ed445
shirin-sha <project.manager4565@gmail.com>
2026-09-24 11:10:03 +0400

    testimonial

--- src/components/sections/testimonials/Testimonials7.js
+++ src/components/sections/testimonials/Testimonials7.js
@@ -24,7 +24,7 @@ const Testimonials7 = () => {
 	}, []);
 
 	return (
-		<section className="h8-testimonial mb-60">
+		<section className="h8-testimonial">
 			<div className="container tj-gap-30">
 				<div className="row">
 					{/* Left column */}
